eebe68a73a9844e3dc35d479e312f76b0721a38f
jason yang <jhyapril@gmail.com>
2018-07-06 11:32:36 +0800

    updated mock

--- src/components/Constants/mockData.js
+++ src/components/Constants/mockData.js
@@ -1,12 +1,14 @@
 const data = [
   {
     id: "123123",
+    login: "zpao",
     name: "Paul O’Shannessy",
     avatar_url: "https://avatars1.githubusercontent.com/u/8445?v=4",
     company: "Facebook",
     created_at: "2008-04-25"
   } , {
     id: "45345234",
+    login: "balpert",
     name: "Ben Alpert",
     avatar_url: "https://avatars0.githubusercontent.com/u/7585659?v=4",
     company: "Facebook",
