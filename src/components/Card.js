97b680eeb0dd87ba1811f01e1de179c7605ecbf7
jason yang <jhyapril@gmail.com>
2018-07-06 11:32:26 +0800

    display username

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,10 +5,11 @@ const Card = (props) => {
     <div style={{margin: '1em'}}>
       <img width="75" src={props.avatar_url} />
       <div style={{display: 'inline-block', marginLeft:10}}>
+        <div>{props.login}</div>
         <div style={{fontSize: '1.25em', fontWeight: 'bold'}}>
           {props.name}
         </div>
-        <div>{props.company}</div>
+        <div>{props.company || 'Unknown Company'}</div>
         <div>Joined at: {props.created_at.substring(0, 10)}</div>
       </div>
     </div>
@@ -16,6 +17,7 @@ const Card = (props) => {
 };
 
 Card.propTypes = {
+  login: PropTypes.string.isRequired,
   avatar_url: PropTypes.string,
   name: PropTypes.string,
   company: PropTypes.string,
